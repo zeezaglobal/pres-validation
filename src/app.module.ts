4d8d66a1d5529abce0b4cd544f1099d2c9330ec3
Rahulraj sr <rahulsr2714@gmail.com>
2025-07-04 18:18:32 +0530

    Implimented configurations in the project

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,22 +4,31 @@ import { AppService } from './app.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Prescription } from './entities/entities/Prescription.entity';
 import { Doctor } from './entities/entities/Doctor.entity';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 
 @Module({
   imports: [
-    TypeOrmModule.forRoot({
-      type: 'mysql',
-      host: '147.93.43.194',
-      port: 3306,
-      username: 'readonly_user',
-      password: 'strong_password',
-      database: 'prescription',
-      entities: [`${__dirname}/**/*.entity{.ts,.js}`],
-      synchronize: false,
-      extra: {
-        charset: 'utf8mb4_general_ci',
-        ssl: false,
-      },
+    ConfigModule.forRoot({
+      isGlobal: true,
+      envFilePath: '.env',
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'mysql',
+        host: configService.get<string>('DB_HOST'),
+        port: 3306,
+        username: configService.get<string>('DB_USERNAME'),
+        password: configService.get<string>('DB_PASSWORD'),
+        database: configService.get<string>('DB_NAME'),
+        entities: [`${__dirname}/**/*.entity{.ts,.js}`],
+        synchronize: false,
+        extra: {
+          charset: 'utf8mb4_general_ci',
+          ssl: configService.get<string>('DB_SSL') == 'true' ? true : false,
+        },
+      }),
     }),
     TypeOrmModule.forFeature([Prescription, Doctor]),
   ],
